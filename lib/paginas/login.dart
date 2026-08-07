cee49abbe9a17e46a5ccd870d2431e209133063a
agatafontes <fontesgomesagata@gmail.com>
2026-08-06 21:31:48 -0300

    cria uma conexao com o servidor via backend
    - faz uma requisição com o http
    - ele muda de login para dashboard

--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -1,34 +1,53 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:primeiro_app/paginas/cadastro.dart';
 import 'package:primeiro_app/utilitarios/tipografia.dart';
+import 'package:http/http.dart' as http;
 
 import 'dashboard.dart';
+
+
 
 class Login extends StatefulWidget {
   const Login({super.key});
 
-
-
   @override
   State<Login> createState() => _LoginState();
 }
-
 class _LoginState extends State<Login> {
   final emailControlador = TextEditingController();
   final senhaControlador = TextEditingController();
-  
-  void fazerLogin(){
-    if (emailControlador.text != "teste@email.com" ||
-        senhaControlador.text != "123456" ){
+
+
+  Future<void> fazerLogin() async {
+    var url = Uri.http("10.112.4.33", "login");
+    var resposta = await  http.post(
+      url,
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: jsonEncode({
+          'email': emailControlador.text,
+          'senha': senhaControlador.text
+        }
+      ),
+    );
+
+    if(resposta.statusCode != 200 ){
+      var dados = jsonDecode(resposta.body);
+
       ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Email e/ou senha estão incorretos!")),
+        SnackBar(content: Text("${dados["message"]}")),
       );
       return;
     }
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (build) => Dashboard()),
-    );
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (build) => Dashboard()),
+      );
   }
   
   
